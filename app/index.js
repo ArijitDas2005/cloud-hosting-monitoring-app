184a0f47e3585bc2bcafe3f09dbafd138b493527
Arijit <rumadas.r123@gmail.com>
2025-06-27 09:06:04 +0530

    Remove .pem file and update .gitignore

---
+++ app/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('🚀 Hello from Cloud Hosted Node.js App!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
